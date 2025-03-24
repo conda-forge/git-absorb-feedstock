fe0c8148b59382c1a580b13c53e4c1ecf88adc78
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-24 16:23:17 -0700

    skip man build for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,8 +17,6 @@ if [[ ${build_platform} == ${target_platform} ]]; then
     mkdir -p ${PREFIX}/etc/bash_completion.d
     mkdir -p ${PREFIX}/share/fish/vendor_completions.d
     mkdir -p ${PREFIX}/share/zsh/site-functions
-    mkdir -p ${PREFIX}/share/man/man1
-    install -m 644 Documentation/git-absorb.1 ${PREFIX}/share/man/man1/git-absorb.1
     git-absorb --gen-completions bash > ${PREFIX}/etc/bash_completion.d/git-absorb
     git-absorb --gen-completions fish > ${PREFIX}/share/fish/vendor_completions.d/git-absorb.fish
     git-absorb --gen-completions zsh > ${PREFIX}/share/zsh/site-functions/_git-absorb
